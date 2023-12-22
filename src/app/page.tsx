3685f323fa0b53d4a5d09a7b85b71ea3cfbc2725
kidmortal <kidmortal@gmail.com>
2023-12-22 16:02:32 -0300

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { UploadForm } from "@/components/UploadForm";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <UploadForm />
-      </div>
-    </main>
+    <div className={styles.container}>
+      <UploadForm />
+    </div>
   );
 }
