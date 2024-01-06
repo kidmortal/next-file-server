2c1b18adfeff559d5b9ccec135c892761c5243aa
kidmortal <kidmortal@gmail.com>
2024-01-05 22:01:44 -0300

    num sei

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import useStore from "@/store/main";
+import { Icon, IconButton, Stack } from "@chakra-ui/react";
 import { usePathname } from "next/navigation";
+import { PiSignOutBold } from "react-icons/pi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { auth } from "@/firebase";
 
 export const HeaderComponent = () => {
+  const store = useStore();
   const pathname = usePathname();
 
   function switchPageTitle() {
     switch (pathname) {
-      case "/":
+      case "/dash":
         return "Visão geral";
-      case "/stock":
+      case "/dash/stock":
         return "Estoque de produtos";
-      case "/stockrules":
+      case "/dash/stockrules":
         return "Regra de estoque";
-      case "/settings":
+      case "/dash/settings":
         return "Configurações";
 
       default:
@@ -21,5 +27,19 @@ export const HeaderComponent = () => {
     }
   }
 
-  return <>{switchPageTitle()}</>;
+  return (
+    <Stack direction="row" justifyContent="space-between">
+      <span>{switchPageTitle()}</span>
+      <Stack direction="row" align="center">
+        <Icon as={FaRegUserCircle} width={25} height={25} />
+        <strong>{store.user?.email}</strong>
+        <IconButton
+          aria-label="signout"
+          backgroundColor="transparent"
+          onClick={() => auth.signOut()}
+          icon={<Icon as={PiSignOutBold} width={25} height={25} color="red" />}
+        />
+      </Stack>
+    </Stack>
+  );
 };
